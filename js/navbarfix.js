686e11518b89606c15e640a39d2eae1eeb484be4
Alfred0 <90577614+Alf3rez@users.noreply.github.com>
2025-02-17 19:02:44 +0100

    Visual fixes
    Updated javascript for fixing navbar behavior in different scenarios

--- js/navbarfix.js
+++ js/navbarfix.js
@@ -10,6 +10,16 @@ window.addEventListener('scroll', () => {
     if (navbarCollapse.classList.contains('show')) {
         $(navbarCollapse).collapse('hide'); // Usar jQuery para colapsar
     }
+	
+	// Verificar el ancho de la ventana
+    if (window.innerWidth > 1023) {
+        // Aquí puedes agregar la lógica para mantener la clase '-pinned'
+        // Por ejemplo, agregar la clase a la barra de navegación
+        document.querySelector('.navbar').classList.add('-pinned');
+    } else {
+        // Si el ancho es menor o igual a 1023px, puedes quitar la clase
+        document.querySelector('.navbar').classList.remove('-pinned');
+    }	
 });
 
 // Colapsar el menú al hacer clic en un elemento del menú
